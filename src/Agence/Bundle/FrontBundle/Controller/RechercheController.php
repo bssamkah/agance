a9daabc6389ea84e5329f35f234fbfe84b3af55d
samirchandoul <samir.chandoul@gmail.com>
2015-04-02 06:57:35 +0200

    recherche index

--- src/Agence/Bundle/FrontBundle/Controller/RechercheController.php
+++ src/Agence/Bundle/FrontBundle/Controller/RechercheController.php
@@ -28,78 +28,26 @@ class RechercheController extends Controller {
          $request = $this->getRequest();
       
         $chambre = $request->query->get('chambre');
-        $etage = $request->query->get('etage');
-        $prix = $request->query->get('date');
-        $type = $request->query->get('type');
+         $etage = $request->query->get('etage');
         $offres = NULL;
         $em = $this->getDoctrine()->getEntityManager();
        
        
         if ($chambre != null) {
-            $resultats = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('nbrChambre' => $chambre));
-            if ($resultats != null) {
-                foreach ($resultats as $resultat) {
-                    foreach ($resultat as $off) {
-                        $offres[] = $off;
-                    }
-                }
-            } else
-            // $events = $em->createQuery("SELECT e FROM FrontOfficeOptimusBundle:Event e WHERE e.createur='' " );
-                $offres = NULL;
-            // die ("inexistant")
-            ;
-        }
-        if ($etage != null) {
-            $resultats = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('etage' => $etage));
-            if ($resultats != null) {
-                foreach ($resultats as $resultat) {
-                    foreach ($resultat as $off) {
-                        $offres[] = $off;
-                    }
-                }
-            } else
-            // $events = $em->createQuery("SELECT e FROM FrontOfficeOptimusBundle:Event e WHERE e.createur='' " );
-                $offres = NULL;
-            // die ("inexistant")
-            ;
-        }
-        if ($prix != null) {
-            $resultats = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('prix' => $prix));
-            if ($resultats != null) {
-                foreach ($resultats as $resultat) {
-                    foreach ($resultat as $off) {
-                        $offres[] = $off;
-                    }
-                }
-            } else
-            // $events = $em->createQuery("SELECT e FROM FrontOfficeOptimusBundle:Event e WHERE e.createur='' " );
-                $offres = NULL;
-            // die ("inexistant")
-            ;
-        }
-         if ($type != null) {
-            $resultats = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('type' => $type));
-            if ($resultats != null) {
-                foreach ($resultats as $resultat) {
-                    foreach ($resultat as $off) {
-                        $offres[] = $off;
-                    }
-                }
-            } else
-            // $events = $em->createQuery("SELECT e FROM FrontOfficeOptimusBundle:Event e WHERE e.createur='' " );
-                $offres = NULL;
-            // die ("inexistant")
-            ;
-        }
-
-
+            $offres = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('nbrChambre' => $chambre));
+           
+            }
+            if ($etage != null) {
+            $offres = $em->getRepository("AgenceFrontBundle:Offre")->findBy(array('etage' => $etage));
+           
+            }
+            
+       
        
 
         $res['offres'] = $offres;
         $res['chambre'] = $chambre;
-        $res['etage'] = $etage;
-        $res['prix'] = $prix;
-        $res['type'] = $type;
+       
       
         $res['nbr_offres'] = count($res['offres']);
         return $this->render('AgenceFrontBundle:Recherche:searechIndex.html.twig',array('res' => $res));
