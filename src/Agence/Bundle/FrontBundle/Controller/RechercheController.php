c16fd70867517c823185ec1b407c2f59c8c4206e
samirchandoul <samir.chandoul@gmail.com>
2015-04-04 02:29:54 +0200

    recherche

--- src/Agence/Bundle/FrontBundle/Controller/RechercheController.php
+++ src/Agence/Bundle/FrontBundle/Controller/RechercheController.php
@@ -19,9 +19,56 @@ class RechercheController extends Controller {
 
    
     public function rechercheAction() {
-        $em = $this->getDoctrine()->getManager();
+         $request = $this->getRequest();
+      
+        $chambre = $request->query->get('chambre');
+         $etage = $request->query->get('etage');
+          $type = $request->query->get('type');
+          $prix = $request->query->get('prix');
+         
+        $offe = NULL;
+       
+       $condition = "WHERE 1=1";
+        $parameters = array();
+       
+       if ($chambre != null) {
+            $condition = $condition . " AND o.nbrChambre = :chambre";
+            $parameters[':chambre'] = $chambre;
+        }
+        if ($etage != null) {
+            $condition = $condition . " AND UPPER(o.etage) LIKE :etage";
+            $parameters[':etage'] = $etage;
+            
+        }
+        if ($type != null) {
+            $condition = $condition . " AND UPPER(o.type) LIKE :type";
+            $parameters[':type'] = $type;
+        }
+        if ($prix != null) {
+            $condition = $condition . " AND o.prix = :prix ";
+            $parameters[':prix'] = $prix;
+           
+            
+        }
+        
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        if($chambre || $etage || $type || $prix != Null){
+           $offress = $em->createQuery("SELECT o FROM AgenceFrontBundle:Offre o " . $condition);
+            $offress->setParameters($parameters);
+            $offe = $offress->getResult();
+        }
+        $res['offres'] = $offe;
+        $res['chambre'] = $chambre;
+         $res['etage'] = $etage;
+          $res['type'] = $type;
+           $res['prix'] = $prix;
+           
+       
+      
+        $res['nbr_offres'] = count($res['offres']);
         $offres = $em->getRepository("AgenceFrontBundle:Offre")->findAll(array('createdAt' => 'DESC'));
-        return $this->render('AgenceFrontBundle:Recherche:recherche.html.twig',array('offres' => $offres));
+        return $this->render('AgenceFrontBundle:Recherche:recherche.html.twig',array('offres' => $offres,'res' => $res));
         
     }
     public function rechercheIndexAction() {
